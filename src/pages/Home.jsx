32e66d3dd0db5d14c4d8f01182fce901ca8c604c
reynol-31 <reynol.23cs119@sode-edu.in>
2026-04-23 15:44:18 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Pipeline from "../components/Pipeline";
+import Results from "../components/Results";
+import Contact from "../components/Contact";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <About />
+      <Pipeline />
+      <Results />
+      <Contact />
+    </>
+  );
+}
